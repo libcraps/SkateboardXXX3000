3f3e354fdf79cf8468e64a13ec3c466a88333d9b
libcraps <56928268+libcraps@users.noreply.github.com>
2021-01-15 18:23:42 +0100

    test import pandeas

--- Main_Movuina.py
+++ Main_Movuina.py
@@ -3,6 +3,7 @@ import time
 from threading import Thread
 import Movuino as mvn
 import matplotlib.pyplot as plt
+import pandas
 # ok ok
 #####################################################################
 #####################		MOVUINO			#########################
