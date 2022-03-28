e07bf655b20ebdcc2bfa8de8bb018b34e7e3766f
libcraps <56928268+libcraps@users.noreply.github.com>
2022-03-28 10:11:47 +0200

    Create tricksRecognition.py

---
+++ 01 - Python/MovuinoDataHandler/tricksRecognition.py
@@ -0,0 +1,36 @@
+import dataSet.SkateboardXXX3000DataSet as sk
+import pandas as pd
+import tools.signalAnalysis as sa
+import tools.displayFunctions as df
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
+
+from scipy.signal import find_peaks
+
+from main_isolation_tricks import eventDetection, centerEvents
+
+############   SETTINGS   #############
+completeSequencesPath = "..\\..\\06 - Data\\Raw_sequences\\sesh_151121_\\record_9_interpolated.csv"
+
+
+#--- Opening file ---
+print("Opening : " + completeSequencesPath)
+completeSequence = sk.SkateboardXXX3000DataSet(completeSequencesPath)
+Te = completeSequence.Te
+print("sample period : " + str(Te))
+print("sample frequency : " + str(1 / Te))
+
+#------- PEAK DETECTION ----------
+size_window = int(1/Te)
+overlap = int(size_window // 2)
+
+prominence = 3
+distance = 4
+
+#----------------
+
+time_win, tricks_interval_temp, peaks_gyr, peak_a, peaks_tricks = eventDetection(completeSequence, size_window,overlap,prominence,distance)
+
+tricks_interval = centerEvents(completeSequence,tricks_interval_temp)
